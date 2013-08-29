234f8be37e435133bddb366c298a8e29a20c8b92
Aurelie Aubertin <aurelie@morphoss.com>
2013-08-29 14:06:09 +1200

    replace the copyright 2011 by 2013

--- src/com/morphoss/xo/jumble/videos/HomeEndActivity.java
+++ src/com/morphoss/xo/jumble/videos/HomeEndActivity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 Morphoss Ltd
+ * Copyright (C) 2013 Morphoss Ltd
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
